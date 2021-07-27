607eb4538816b83643834bc714251b0d4bf9707b
Grzegorz Dziubak <grzegorzdziubak@gmail.com>
2021-07-27 11:22:03 +0200

    Not used jet

--- src/de/garden/frontend/info/MyJLabel.java
+++ src/de/garden/frontend/info/MyJLabel.java
@@ -3,23 +3,59 @@ package de.garden.frontend.info;
 import java.awt.Graphics;
 
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
+/**
+ * My attempt to overwrite the standard class JLabel,
+ * not fully implemented<br><br>
+ * 
+ * @author grzeg
+ *
+ */
 public class MyJLabel extends JLabel {
 	
-	String text;
+//	*******************************************
+//	*				  Attributs
+//	*******************************************
 	
+	/**
+	 * Standard serial UID
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Returned text
+	 */
+	private String text;
+	
+//	*******************************************
+//	*				Konstruktors
+//	*******************************************
+	
+	/**
+	 * Class constructor
+	 * @param text
+	 */
 	public MyJLabel(String text) {
 		super(text);
 		this.text = text;
 	}
 
+//	*******************************************
+//	*				 Functions
+//	*******************************************
+	
+	/**
+	 * My attempt to override paint function
+	 */
 	@Override
 	public void paint(Graphics e) {
 		super.paint(e);
 		e.drawString(text, 20, 20);
 	}
 	
+	/**
+	 * Getter toString()
+	 */
 	public String toString() {
 		return text;
 	}
