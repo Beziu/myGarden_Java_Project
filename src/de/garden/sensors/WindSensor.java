d9e4ebfef56ef6e8fb9ff419036bef6906deff80
Grzegorz Dziubak <grzegorzdziubak@gmail.com>
2021-08-02 17:50:28 +0200

    Adds constructors and methods

--- src/de/garden/sensors/WindSensor.java
+++ src/de/garden/sensors/WindSensor.java
@@ -1,11 +1,37 @@
 package de.garden.sensors;
 
+import java.util.Random;
+
 public class WindSensor {
+	
+//	*******************************************
+//	*				  Attributs
+//	*******************************************
 	
 	protected int rpms;
 	
 	protected boolean wind;
-
+	
+//	*******************************************
+//	*				Konstruktors
+//	*******************************************
+	
+	public WindSensor(int rpms, boolean wind) {
+		super();
+		this.rpms = rpms;
+		this.wind = wind;
+	}
+	
+	public WindSensor() {
+		Random lotto= new Random();
+		this.rpms = lotto.nextInt(200);;
+		this.wind = lotto.nextBoolean();
+	}
+	
+//	*******************************************
+//	*				 Functions
+//	*******************************************
+	
 	public int getRpms() {
 		return rpms;
 	}
@@ -20,6 +46,11 @@ public class WindSensor {
 
 	public void setWind(boolean wind) {
 		this.wind = wind;
+	}
+
+	@Override
+	public String toString() {
+		return "WindSensor [rpms=" + rpms + ", wind=" + wind + "]";
 	}
 
 }
