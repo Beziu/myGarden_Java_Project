0900f56b609dd1b4b5b55a7a802b186a19412a70
Grzegorz Dziubak <grzegorzdziubak@gmail.com>
2021-08-04 09:15:53 +0200

    New class

---
+++ src/de/garden/frontend/WindSensorTab.java
@@ -0,0 +1,35 @@
+package de.garden.frontend;
+
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
+public class WindSensorTab extends JPanel {
+
+//	*******************************************
+//	*				  Attributs
+//	*******************************************
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+//	*******************************************
+//	*				Konstruktors
+//	*******************************************
+	
+	public WindSensorTab() {
+		this.setLayout(new GridLayout(1, 2, 5, 5));
+		build();
+	}
+
+//	*******************************************
+//	*				 Functions
+//	*******************************************
+	
+	private void build() {
+		
+		
+	}
+}
